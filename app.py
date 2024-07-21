e4c7678baa668b096d8b781d946cdcc74fffc954
Brook <storyofasword@hotmail.com>
2024-07-21 13:56:56 -0400

    changed single quote for main to double quote

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, world!</p>"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True) 
     # debug=True enables debug mode which provides more detailed error messages when something goes wrong. Also allows for "hot-reloading" which means the server will automatically update when making changes to the code. It is important to turn this off when the project is finished and we are ready to deploy.
 
-    
