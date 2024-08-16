52d07a3596183b852d41f045fc10e2ace78502de
Brook <storyofasword@hotmail.com>
2024-08-16 17:52:24 -0400

    side panel holds elements correctly, sidebar hamburger icon opens side panel correctly and also closes it correctly.

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,3 +1,7 @@
-document.querySelector('.navbar-toggler').addEventListener('click', function () {
-    document.getElementById('sidebar-container').classList.toggle('expanded');
-});
+// document.querySelector('.navbar-toggler').addEventListener('click', function () {
+//     document.getElementById('sidebar-container').classList.toggle('expanded');
+// });
+
+document.querySelector('.collapsed-sidebar').addEventListener('click', function () {
+    document.querySelector('.vertical-column-wrapper').classList.toggle('collapsed')
+})
